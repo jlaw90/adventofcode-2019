587867681287b2c1398cd284c7d7b24475abdf46
James Lawrence <jlaw90@hotmail.com>
2019-12-10 22:01:12 +0000

    Almost, but not quite...

--- 10/part1.rb
+++ 10/part1.rb
@@ -8,8 +8,6 @@ until (line = gets.strip).empty?
 end
 height = y
 
-puts asteroids.to_s
-
 asteroids.keys.each_with_index do |a, i|
   ax, ay = a
   asteroids.keys[(i+1)..-1].each_with_index do |b, j|
@@ -18,19 +16,36 @@ asteroids.keys.each_with_index do |a, i|
     dx, dy = bx - ax, by - ay
     m = if dx == 0 then 0.to_f else dy.to_f / dx.to_f end
     c = ay.to_f - m * ax.to_f
-    blocked = false
-    for x in if ax < bx then (ax+1)...bx else (ax-1).downto(bx) end
-      y = m * x.to_f + c
-      puts "#{i}: #{ax},#{ay} to #{j}: #{x},#{y}: #{if asteroids.has_key? [x,y] then 'blocked' else '' end}"
-      next if y % 1 != 0
-      y = y.to_i
-      blocked = true if asteroids.has_key? [x, y]
+    sparse = dx < dy
+
+    range = if sparse then if ay < by then (ay+1)...by else (ay-1).downto(by+1) end elsif ax < bx then (ax+1)...bx else (ax-1).downto(bx+1) end
+    transform = lambda { |v| if sparse then [if m == 0 then ax else (v.to_f - c) / m end, v] else [v, m * v.to_f + c] end }
+
+    if range.all? do |v|
+        x, y = transform.call(v)
+        possible = x % 1 == 0 && y % 1 == 0
+        blocked = possible && asteroids.has_key?([x.to_i, y.to_i])
+        !blocked
     end
-    unless blocked
+
       asteroids[a] += 1
       asteroids[b] += 1
     end
   end
 end
 
-puts asteroids.to_s
+winner = asteroids.keys[asteroids.values.each_with_index.max[1]]
+puts "#{winner[0]},#{winner[1]}"
+
+chars = 4
+format = "%#{chars}.#{chars}s"
+for y in 0...height
+  for x in 0...width
+    print(if asteroids[[x, y]]
+            format % asteroids[[x, y]].to_s
+          else
+            format % '.'
+          end)
+  end
+  puts
+end
