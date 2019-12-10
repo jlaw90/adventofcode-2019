af630a72511347b04257957d489020299f292961
James Lawrence <j.lawrence@cloudfmgroup.com>
2019-12-10 16:43:43 +0000

    Some better code, cannot debug from work...

--- 10/part1.rb
+++ 10/part1.rb
@@ -8,14 +8,29 @@ until (line = gets.strip).empty?
 end
 height = y
 
+puts asteroids.to_s
+
 asteroids.keys.each_with_index do |a, i|
   ax, ay = a
-  asteroids.keys[(i+1)..-1].each do |b|
+  asteroids.keys[(i+1)..-1].each_with_index do |b, j|
     bx, by = b
 
     dx, dy = bx - ax, by - ay
-    slope = if dx == 0 then dy / dx else 0 end
-    b = ay-slope*ax
-
+    m = if dx == 0 then 0.to_f else dy.to_f / dx.to_f end
+    c = ay.to_f - m * ax.to_f
+    blocked = false
+    for x in if ax < bx then (ax+1)...bx else (ax-1).downto(bx) end
+      y = m * x.to_f + c
+      puts "#{i}: #{ax},#{ay} to #{j}: #{x},#{y}: #{if asteroids.has_key? [x,y] then 'blocked' else '' end}"
+      next if y % 1 != 0
+      y = y.to_i
+      blocked = true if asteroids.has_key? [x, y]
+    end
+    unless blocked
+      asteroids[a] += 1
+      asteroids[b] += 1
+    end
   end
 end
+
+puts asteroids.to_s
